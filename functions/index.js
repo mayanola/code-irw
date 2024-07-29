4278052255cb5448a3d1a2c36c6d450565911b7c
maya177 <mayaNbridgman@gmail.com>
2024-07-29 11:36:32 +0100

    initial commit setting up firebase and react

---
+++ functions/index.js
@@ -0,0 +1,66 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+const functions = require('firebase-functions/v1');
+const cors = require('cors')({ origin: true });
+require('dotenv').config();
+
+
+// Setup the OpenAI API
+// const API_KEY = "sk-proj-0MAh3JhaG59omGtWDpzJT3BlbkFJKj1tH72rspC2sFyhymHT";
+const OpenAI = require('openai');
+const openai = new OpenAI({ apiKey: process.env.CHATGPT_API_KEY });
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const systemMessage = {
+  "role": "system",
+  "content": "Explain all concepts like you are a pirate."
+}
+
+// Take the text parameter passed to this HTTP endpoint and insert it into
+// Firestore under the path /messages/:documentId/original
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+  cors(req, res, async() => {
+
+   // Grab the text parameter.
+   //try {
+    const original = req.body.data.text;
+
+    const userMessage = {
+        role: 'user',
+        content: original
+      };
+
+    // Call GPT API
+    const completions = await openai.chat.completions.create({
+        messages: [
+          systemMessage,
+          userMessage
+        ],
+        model: "gpt-4o",
+      });
+      const apiResponse = (completions.choices[0].message.content);
+    
+    const writeResult = await admin
+     .firestore()
+     .collection("messages")
+     .add({
+      message: apiResponse || null,
+      sender: "assistant",
+      timestamp: Date.now()
+    });
+
+    const writeResult2 = await admin
+     .firestore()
+     .collection("messages")
+     .add({ original: original || null,
+        sender: "user",
+        timestamp: Date.now()
+     });
+
+    // Send back a message that we've successfully written the message
+    // need to get bck the uppercase field from the document in db
+    res.status(200).json({ result: `Message with ID: ${writeResult.id} added.` });
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Step1 from './components/Step1';
+import Step2 from './components/Step2';
+import Step3 from './components/Step3';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Step1} />
+        <Route path="/step2" component={Step2} />
+        <Route path="/step3" component={Step3} />
+      </Switch>
+    </Router>
   );
 }
 
